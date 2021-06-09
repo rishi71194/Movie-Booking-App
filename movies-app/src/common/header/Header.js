94692bf312f1681b8f2ab439862798e1fac7634f
rbanerjee71194 <rbanerjee71194@gmail.com>
2021-06-10 00:15:55 +0530

    second commit

---
+++ movies-app/src/common/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component{
+
+    render(){
+        return (
+        <div className = "header">
+            <img src = "https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg" className="logo" alt="logo"/>
+        </div>
+        
+        );
+    }
+}
+
+export default Header;
